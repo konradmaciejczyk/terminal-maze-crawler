462a786214b07af7d0490ff14dbcd0d701af5359
Konrad Maciejczyk <conradmac97@gmail.com>
2020-11-15 23:58:39 +0100

    implemented main menu, added credits screen, added quit game option

--- main.c
+++ main.c
@@ -1,13 +1,30 @@
+//Konrad Maciejczyk, 2020
 #include <stdlib.h>
-#include <string.h>
 #include <ncurses.h>
+#include <menu.h>
+void credits();
 
+void set_game_enviroment(){
+   system("printf '\e[8;50;150t'"); //Setting user terminal to 150x50
 
-int main() {
+   initscr(); //Initialize ncurses screen
 
-   system("printf '\e[8;50;150t'");
+   if (has_colors() == false){ //Check whether terminal can display colors
+      printf("To play this game your terminal must support color display.");
+      exit(1);
+   }
 
-   int title_len = 94;
+   start_color(); //start colorful display
+   init_pair(1, COLOR_WHITE, COLOR_BLACK); //Setting background color
+}
+
+void quit_game(){
+   endwin();
+   exit(0);
+}
+
+void display_banner(){
+   int title_width = 94, title_height = 18;
    char title[] = "   _____                             _                          _                _           \n"
                   "  / ____|                           | |                        (_)              | |          \n"
                   " | (___   _   _  _ __    ___  _ __  | |_  ___  _ __  _ __ ___   _  _ __    __ _ | |          \n"
@@ -21,22 +38,116 @@ int main() {
                   " | (_| || |_| || | | || (_| ||  __/| (_) || | | | | (__ | |  | (_| | \\ V  V / | ||  __/| |   \n"
                   "  \\__,_| \\__,_||_| |_| \\__, | \\___| \\___/ |_| |_|  \\___||_|   \\__,_|  \\_/\\_/  |_| \\___||_|   \n"
                   "                        __/ |                                                                \n"
-                  "                       |___/                                                                 \n";
+                  "                       |___/                                                                 ";
 
 
+   
+   int row, col; getmaxyx(stdscr, row, col); //Getting current terminal height, width
 
-   initscr();
-   int row, col;
-   getmaxyx(stdscr, row, col);
-   mvprintw((row/2) -10, title_len/94, "%s", title);
-   refresh();			/* Print it on to the real screen */
-	getch();			/* Wait for user input */
-	endwin();			/* End curses mode		  */
+   int banner_pos_x = col/2 - title_width / 2;
+   int banner_pos_y = row/2 - title_height;
+   int banner_width = title_width + 3;
+   int banner_height = title_height +3;
 
-	return 0;
+   WINDOW *banner = newwin(title_height, title_width, banner_pos_y, banner_pos_x); //Initialize window for title banner
+   WINDOW *banner_foreground = newwin(banner_height, banner_width, banner_pos_y - 2, banner_pos_x - 2); //Initialize window for border under title
+   refresh();  
 
+   start_color();
+   init_pair(2, COLOR_RED, COLOR_BLACK);//Setting 
+   box(banner_foreground, 0, 0);
+   wrefresh(banner_foreground); //Showing border around title banner   
+   
+   wattron(banner, COLOR_PAIR(2)); wattron(banner, A_BOLD);
+   wprintw(banner, title); //Showing banner
+   wrefresh(banner);	  
+   wattroff(banner, COLOR_PAIR(2)); wattroff(banner, A_BOLD);	
+}
 
+void main_menu(){
+   int row, col; getmaxyx(stdscr, row, col);
 
+   int menu_width = 50, menu_height = 10, menu_pos_x = col/2 - 25, menu_pos_y = row - 22; //Creating window for opton menu_win
+   WINDOW *menu_win = newwin(10, 50, menu_pos_y, menu_pos_x);
+   refresh();
+   wrefresh(menu_win);
 
-  
+   keypad(menu_win, true); //Allows to use arrows in order to pick up options from menu
+
+   char MENU_OPTIONS[4][13] = {"New Game", "Hall of Fame", "Credits", "Quit Game"};
+
+   int highlight = 0, choice;
+
+   while(1){
+      for (int i=0; i<4; i++){
+         if(i==highlight){
+            wattron(menu_win, A_REVERSE);
+         }
+         
+         mvwprintw(menu_win, i+1, 19, MENU_OPTIONS[i]);       
+         wattroff(menu_win, A_REVERSE);       
+      }
+         choice = wgetch(menu_win);  
+
+         switch (choice)
+         {
+         case KEY_UP:
+            highlight--;
+            if (highlight == -1)
+               highlight = 0;
+            break;
+         case KEY_DOWN:
+            highlight++;
+            if (highlight == 4)
+               highlight = 3;
+            break;         
+         default:
+            break;
+         }
+         if(choice == 10){
+            break;
+         }
+   }
+
+   switch (highlight)
+   {
+   case 0:
+      printf("New Game goes here");
+      break;
+   case 1:
+      printf("Hall of Fame goes here");
+      break;
+   case 2:
+      credits();
+   case 3:
+      quit_game();
+
+   
+   default:
+      break;
+   }
+
+    
+}
+
+void credits(){
+   clear();
+   mvprintw(22, 71, "Made by:");
+   mvprintw(23, 63, "Konrad Maciejczyk, 2020");
+   mvprintw(27, 61, "github.com/konradmaciejczyk");
+   getch();
+   clear();
+   display_banner();
+   main_menu();
+
+}
+
+int main() {
+   set_game_enviroment();
+   display_banner();
+   main_menu();
+   
+   getch();		
+	endwin();
+	return 0;  
 }
